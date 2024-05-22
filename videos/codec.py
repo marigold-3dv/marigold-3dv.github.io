7e2fc3df9b83a888ead1b057866e9b37a86f370c
Xiangyi Jia <1816020611@qq.com>
2024-05-22 22:42:13 +0200

    videos for phone

--- videos/codec.py
+++ videos/codec.py
@@ -6,7 +6,8 @@ def main():
     for _, _, files in os.walk('.'):
         for file in files:
             # ffmpeg -i input_video.mp4 -c:v libx264 -crf 23 -preset medium output_video.mp4
-            sp.run(['ffmpeg', '-i', file, '-c:v', 'libx264', '-crf', '0', '-preset', 'veryslow', 'h264_' + file])
+            # for phone: ffmpeg -i fern.mp4 -c:v libx264 -crf 23 -preset medium -profile:v baseline -level 3.0 -c:a aac -strict -2 output.mp4
+            sp.run(['ffmpeg', '-i', file, '-c:v', 'libx264', '-crf', '23', '-preset', 'medium', '-profile:v', 'baseline', '-level', '3.0', '-c:a', 'aac', '-strict', '-2', 'h264_' + file])
 
 if __name__ == '__main__':
     main()
